d80750e7a04f2d6ffdc63c3024cd3d746704b72b
Chasek99 <ck121642@students.davenport.k12.ia.us>
2017-02-17 14:24:12 -0600

    Add files via upload

---
+++ evenOdds.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package programmingExercises;
+import java.util.Scanner;
+/**
+ * @author CK121642
+ *
+ */
+public class evenOdds {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		  int num;
+		    System.out.println(" PLease enter an Integer number:");
+
+		    //The input provided by user is stored in num
+		    Scanner input = new Scanner(System.in);
+		    num = input.nextInt();
+
+		    /* If number is divisible by 2 then it's an even number
+		     * else odd number*/
+		    if ( num % 2 == 0 )
+		        System.out.println("Entered number is even");
+		     else
+		        System.out.println("Entered number is odd");
+		
+
+	}
+
+}
